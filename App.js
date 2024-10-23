36cd7412c91c45970137bc8d485796be28a75419
mujeebalish <mujeebrehman12122@gmail.com>
2024-10-22 20:53:23 -0700

    home assingment

---
+++ App.js
@@ -0,0 +1,8 @@
+// Age Calcolator
+var userInpAgeAverage = +prompt("Enter age for best ideas food");
+var ageAverageArray = ["New born (0_2 mo) ","Infant (3mo_1yr) ","Toddler (1_4yr)","Child (5_12yr)  ","Teenager/Lover (13_19yr) ","Soldier/Young Adult (20_35yr)","Judge Adult (36_55)","Old man (56_75yr)","Extremeold Age (+76)"]
+for(i=0; i <= ageAverageArray.length; i++){
+if(userInpAgeAverage == "0_2mo"){
+    
+}
+}
